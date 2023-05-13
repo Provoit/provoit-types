47f442643ca0831bf10fd0d641db330d08551902
Valentin271 <valentin.d77@free.fr>
2023-05-13 19:27:06 +0200

    feat: added updatable model

--- src/models.rs
+++ src/models.rs
@@ -1,22 +1,3 @@
-#[cfg(feature = "diesel")]
-use diesel::prelude::*;
+/// This module contains all the application models
 
-#[cfg(feature = "diesel")]
-use crate::schema::*;
-
-use serde::{Deserialize, Serialize};
-
-#[cfg_attr(feature = "diesel", derive(Queryable))]
-#[cfg_attr(feature = "diesel", diesel(table_name = users))]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct User {
-    id: u64,
-    name: String,
-}
-
-#[cfg_attr(feature = "diesel", derive(Insertable))]
-#[cfg_attr(feature = "diesel", diesel(table_name = users))]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct NewUser {
-    name: String,
-}
+pub mod user;
---
+++ src/models/user.rs
@@ -0,0 +1,29 @@
+#[cfg(feature = "diesel")]
+use diesel::prelude::*;
+
+#[cfg(feature = "diesel")]
+use crate::schema::*;
+
+use serde::{Deserialize, Serialize};
+
+#[cfg_attr(feature = "diesel", derive(Queryable))]
+#[cfg_attr(feature = "diesel", diesel(table_name = users))]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct User {
+    id: u64,
+    name: String,
+}
+
+#[cfg_attr(feature = "diesel", derive(Insertable))]
+#[cfg_attr(feature = "diesel", diesel(table_name = users))]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NewUser {
+    name: String,
+}
+
+#[cfg_attr(feature = "diesel", derive(AsChangeset))]
+#[cfg_attr(feature = "diesel", diesel(table_name = users))]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UpdateUser {
+    name: Option<String>,
+}

68e2166b352cfa2c86238e387409182d74729e3e
Valentin271 <valentin.d77@free.fr>
2023-05-22 09:14:08 +0200

    style: code format

--- src/schema.rs
+++ src/schema.rs
@@ -14,7 +14,4 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(
-    users,
-    vehicle_types,
-);
+diesel::allow_tables_to_appear_in_same_query!(users, vehicle_types,);

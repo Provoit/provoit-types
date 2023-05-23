0d96d854a23f032d0315ce5ab067ee4bc30d5451
Valentin271 <valentin.d77@free.fr>
2023-05-23 12:21:29 +0200

    feat: added model

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,4 @@
+pub mod road_types;
 /// This module contains all the application models
 pub mod user;
 pub mod vehicle_types;
---
+++ src/models/road_types.rs
@@ -0,0 +1,14 @@
+#[cfg(feature = "diesel")]
+use diesel::prelude::*;
+
+use serde::{Deserialize, Serialize};
+
+#[cfg_attr(feature = "diesel", derive(Queryable))]
+#[cfg_attr(feature = "diesel", diesel(table_name = vehicle_types))]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RoadType {
+    id: u64,
+    label: String,
+}
+
+// `road_types` is an enumeration table, is shouldn't need to be updated or created via model.

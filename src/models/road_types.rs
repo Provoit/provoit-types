c734d4affa7ae1b5aea10a4f7def26cb3a8e5288
Valentin271 <valentin.d77@free.fr>
2023-05-23 12:23:51 +0200

    fix: copy typo

--- src/models/road_types.rs
+++ src/models/road_types.rs
@@ -4,7 +4,7 @@ use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[cfg_attr(feature = "diesel", derive(Queryable))]
-#[cfg_attr(feature = "diesel", diesel(table_name = vehicle_types))]
+#[cfg_attr(feature = "diesel", diesel(table_name = road_types))]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct RoadType {
     id: u64,

e00f846bba1e2c8230278f4e86d3222a827d3971
Valentin271 <valentin.d77@free.fr>
2023-05-27 18:44:57 +0200

    feat: making attributes public

--- src/models/users.rs
+++ src/models/users.rs
@@ -11,37 +11,37 @@ use serde::{Deserialize, Serialize};
 #[cfg_attr(feature = "diesel", diesel(table_name = users))]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct User {
-    id: u64,
-    firstname: String,
-    lastname: String,
-    mail: String,
-    passwd: String,
-    profile_pic: Option<Blob>,
-    smoker: bool,
-    id_favorite_vehicle: Option<u64>,
+    pub id: u64,
+    pub firstname: String,
+    pub lastname: String,
+    pub mail: String,
+    pub passwd: String,
+    pub profile_pic: Option<Blob>,
+    pub smoker: bool,
+    pub id_favorite_vehicle: Option<u64>,
 }
 
 #[cfg_attr(feature = "diesel", derive(Insertable))]
 #[cfg_attr(feature = "diesel", diesel(table_name = users))]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct NewUser {
-    firstname: String,
-    lastname: String,
-    mail: String,
-    passwd: String,
-    profile_pic: Option<Blob>,
-    smoker: bool,
-    id_favorite_vehicle: Option<u64>,
+    pub firstname: String,
+    pub lastname: String,
+    pub mail: String,
+    pub passwd: String,
+    pub profile_pic: Option<Blob>,
+    pub smoker: bool,
+    pub id_favorite_vehicle: Option<u64>,
 }
 
 #[cfg_attr(feature = "diesel", derive(AsChangeset))]
 #[cfg_attr(feature = "diesel", diesel(table_name = users))]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct UpdateUser {
-    firstname: Option<String>,
-    lastname: Option<String>,
-    mail: Option<String>,
-    profile_pic: Option<Option<Blob>>,
-    smoker: Option<bool>,
-    id_favorite_vehicle: Option<Option<u64>>,
+    pub firstname: Option<String>,
+    pub lastname: Option<String>,
+    pub mail: Option<String>,
+    pub profile_pic: Option<Option<Blob>>,
+    pub smoker: Option<bool>,
+    pub id_favorite_vehicle: Option<Option<u64>>,
 }

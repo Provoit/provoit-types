d7ac4f60950ccc38512fde5259a254b4c1dcbabf
Valentin271 <36198422+Valentin271@users.noreply.github.com>
2023-06-21 17:40:22 +0200

    feat: From trait for NewVehicle (#13)

--- migrations/2023-05-22-061038_create_vehicle_types/up.sql
+++ migrations/2023-05-22-061038_create_vehicle_types/up.sql
@@ -4,3 +4,16 @@ CREATE TABLE `vehicle_types` (
   `id` bigint unsigned PRIMARY KEY AUTO_INCREMENT,
   `label` text NOT NULl
 );
+
+INSERT INTO `vehicle_types`(`id`, `label`)
+VALUES
+(1, "Berline"),
+(2, "Citadine"),
+(3, "Utilitaire"),
+(4, "Franchisseur"),
+(5, "SUV"),
+(6, "Coupé"),
+(7, "Cabriolet"),
+(8, "Break"),
+(9, "Pickup");
+

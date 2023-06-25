1dc12a06b8fa9a78018c131fd2730f7f6a4cba1b
Valentin271 <36198422+Valentin271@users.noreply.github.com>
2023-06-25 16:25:28 +0200

    feat: added week days in database (#14)

--- migrations/2023-05-23-123955_create_days/up.sql
+++ migrations/2023-05-23-123955_create_days/up.sql
@@ -3,3 +3,13 @@ CREATE TABLE `days` (
   `label` text NOT NULL
 );
 
+INSERT INTO `days`(`id`, `label`)
+VALUES
+(1, "Lundi"),
+(2, "Mardi"),
+(3, "Mercredi"),
+(4, "Jeudi"),
+(5, "Vendredi"),
+(6, "Samedi"),
+(7, "Dimanche");
+

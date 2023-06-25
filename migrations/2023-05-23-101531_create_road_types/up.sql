097ae38f5711a0152f8748d6d9206ab357382681
Valentin271 <36198422+Valentin271@users.noreply.github.com>
2023-06-25 16:25:53 +0200

    feat: added road types values (#16)

--- migrations/2023-05-23-101531_create_road_types/up.sql
+++ migrations/2023-05-23-101531_create_road_types/up.sql
@@ -4,3 +4,10 @@ CREATE TABLE `road_types` (
   `id` bigint unsigned PRIMARY KEY AUTO_INCREMENT,
   `label` text NOT NULL
 );
+
+INSERT INTO `road_types`(`id`, `label`)
+VALUES
+(1, "Classique"),
+(2, "Autoroute"),
+(3, "Ferry"),
+(4, "Chemin de terre");
